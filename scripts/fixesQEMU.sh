6cb0af324bce663fa8833ca5d2806e089da654a1
Zdeněk Vydra <485514@mail.muni.cz>
2023-04-19 14:06:09 +0200

    Kali last snapshot

--- scripts/fixesQEMU.sh
+++ scripts/fixesQEMU.sh
@@ -1,5 +1,1 @@
 #!/bin/sh -x
-
-# apt update
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
--- scripts/fixesVBox.sh
+++ scripts/fixesVBox.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -x
 
-# apt update
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-DEBIAN_FRONTEND=noninteractive sudo apt-get update
-
 # install ansible
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y install ansible
 
