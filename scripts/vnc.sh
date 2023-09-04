47dd4ed13fb9bbc0b3415af45d7c82b05c62208d
Zdeněk Vydra <485514@mail.muni.cz>
2023-09-04 11:57:33 +0200

    Comment out vnc fix

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -53,12 +53,12 @@ systemctl start xvnc.socket
 sudo systemctl restart lightdm.service
 
 # Fix "Authentication is required to create a color managed device" popup
-sudo tee -a /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla << EOF
-[Allow Colord all Users]
-Identity=unix-user:*
-Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
-ResultAny=no
-ResultInactive=no
-ResultActive=yes
+# sudo tee -a /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla << EOF
+# [Allow Colord all Users]
+# Identity=unix-user:*
+# Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+# ResultAny=no
+# ResultInactive=no
+# ResultActive=yes
 
-EOF
+# EOF
