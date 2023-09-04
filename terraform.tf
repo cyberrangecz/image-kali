ae80fd64c69f5ee89a1db2758b8b5429c77f652b
Zdeněk Vydra <485514@mail.muni.cz>
2023-09-04 09:12:23 +0200

    Test new CI

--- terraform.tf
+++ terraform.tf
@@ -18,8 +18,7 @@ terraform {
 }
 
 provider "kypo" {
-  endpoint  = local.endpoint
-  client_id = "bzhwmbxgyxALbAdMjYOgpolQzkiQHGwWRXxm"
+  endpoint = var.KYPO_ENDPOINT
 }
 
 provider "openstack" {
@@ -32,9 +31,10 @@ provider "gitlab" {
   token    = var.ACCESS_TOKEN
 }
 
+variable "KYPO_ENDPOINT" {}
 variable "CI_PROJECT_ID" {}
 variable "CI_PROJECT_URL" {}
-variable "CI_COMMIT_SHA" {}
+variable "CI_COMMIT_SHORT_SHA" {}
 variable "CI_SERVER_HOST" {}
 variable "CI_PROJECT_PATH" {}
 variable "NAME" {}
@@ -45,84 +45,19 @@ variable "ACCESS_TOKEN" {
   sensitive = true
 }
 
-locals {
-  endpoint = "https://images.crp.kypo.muni.cz"
-  pool_url = "${local.endpoint}/pool/${module.sandbox.pool_id}"
-}
-
-
-resource "openstack_images_image_v2" "test_image" {
-  name             = "${var.NAME}-${var.CI_COMMIT_SHA}"
-  local_file_path  = "target-qemu/${var.NAME}"
-  container_format = "bare"
-  disk_format      = "raw"
-
-  properties = {
-    hw_scsi_model                          = "virtio-scsi"
-    hw_disk_bus                            = "scsi"
-    hw_rng_model                           = "virtio"
-    hw_qemu_guest_agent                    = "yes"
-    os_require_quiesce                     = "yes"
-    os_type                                = var.TYPE
-    os_distro                              = var.DISTRO
-    "owner_specified.openstack.version"    = var.CI_COMMIT_SHA
-    "owner_specified.openstack.gui_access" = var.GUI_ACCESS
-    "owner_specified.openstack.created_by" = "munikypo"
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      gitlab_branch.gitlab_branch
-    ]
-  }
-}
-
-resource "local_file" "topology" {
-  filename = "topology.yml"
-  content  = replace(file("topology.yml"), "IMAGE_NAME", openstack_images_image_v2.test_image.name)
-}
-
-resource "gitlab_branch" "gitlab_branch" {
-  name    = "test-${var.CI_COMMIT_SHA}"
-  ref     = var.CI_COMMIT_SHA
-  project = var.CI_PROJECT_ID
-}
-
-resource "null_resource" "git_commit" {
-  provisioner "local-exec" {
-    command = <<EOT
-      git config user.name "Service KYPO Images"; git config user.email "485514@muni.cz"
-      git fetch
-      git switch ${gitlab_branch.gitlab_branch.name}
-      git add topology.yml
-      git commit -m "Replace IMAGE_NAME"
-      git push https://root:${var.ACCESS_TOKEN}@${var.CI_SERVER_HOST}/${var.CI_PROJECT_PATH}.git ${gitlab_branch.gitlab_branch.name}
-    EOT
-  }
-
-  triggers = {
-    topology      = local_file.topology.content_sha256
-    gitlab_branch = gitlab_branch.gitlab_branch.name
-  }
-}
-
-module "sandbox" {
-  source      = "vydrazde/sandbox/kypo"
-  project_url = var.CI_PROJECT_URL
-  rev         = var.CI_COMMIT_SHA
-
-  depends_on = [
-    openstack_images_image_v2.test_image,
-    null_resource.git_commit
-  ]
-
-}
-
-resource "local_file" "pool_url" {
-  filename = "vars.env"
-  content  = "POOL_URL=${local.pool_url}"
+module "topology" {
+  source               = "gitlab.ics.muni.cz/muni-kypo-images/image-testing-topology/kypo"
+  kypo_endpoint        = var.KYPO_ENDPOINT
+  project_url          = "${var.CI_SERVER_HOST}/${var.CI_PROJECT_PATH}"
+  project_access_token = var.ACCESS_TOKEN
+  project_id           = var.CI_PROJECT_ID
+  rev                  = var.CI_COMMIT_SHORT_SHA
+  image_name           = var.NAME
+  os_type              = var.TYPE
+  os_distro            = var.DISTRO
+  gui_access           = var.GUI_ACCESS
 }
 
 output "pool_url" {
-  value = local.pool_url
+  value = module.topology.pool_url
 }
