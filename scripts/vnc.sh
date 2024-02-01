8a22380d55b355ee2b36a2c84f70aefbbdde0dc0
Zdeněk Vydra <485514@mail.muni.cz>
2024-02-01 10:33:20 +0100

    Install polkitd-pkla

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -53,7 +53,8 @@ systemctl start xvnc.socket
 sudo systemctl restart lightdm.service
 
 # Fix "Authentication is required to create a color managed device" popup
-sudo tee -a /etc/polkit-1/localauthority.conf.d/45-allow-colord.pkla << EOF
+sudo apt-get install -y polkitd-pkla
+sudo tee -a /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla << EOF
 [Allow Colord all Users]
 Identity=unix-user:*
 Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
